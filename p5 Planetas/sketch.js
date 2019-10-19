4dfe471030a105246ec95849a5ae4fffe2c5d2e7
Unknown <aditooliva@gmail.com>
2019-10-19 00:20:06 -0500

    Añadiendo planetas y órbitas.
    Creando la clase "Planets" para dibujar el ""planeta"" Sol y sus ""lunas"" los planetas. Toda distancia y velocidad de órbitas son aleatorias.

--- p5 Planetas/sketch.js
+++ p5 Planetas/sketch.js
@@ -1,25 +1,61 @@
 let sun;
 
 function setup() {
-  // put setup code here
   createCanvas(400, 400);
-  sun = new Planet(100, 0);
+  sun = new Planet(30, 0);
+  sun.spawnMoons(5);
 }
 
 function draw() {
-  // put drawing code here
   background(0);
+  translate(width/2, width/2)
   sun.show();
+  sun.orbit();
 }
+
 
 class Planet {
 	constructor(r, d) {
 		this.radius = r;
 		this.distance = d;
-		this.angle = 0;
+		this.angle = random(0, 6.28);
+        this.planets = [];
+        this.orbitSpeed = random(0.01, 0.03);
+		//let planets = new Planet[3]
+	}
+
+	orbit() {
+		this.angle = this.angle + this.orbitSpeed;
+		if (this.planets != null) {
+			for (var i = 0; i < this.planets.length; i++){
+				this.planets[i].orbit();
+			}
+		}
+	}
+
+	spawnMoons(Total) {
+		for (var i = 0; i < Total; i++) {
+			var radius = 10 - i;
+			var distance = random(35, 100);
+			this.planets[i] = new Planet(radius, distance);
+		}
 	}
 
 	show() {
+		// Las funciones "push()" y "pop()" son para evitar que
+		// los "translate()" de los planetas se afecten entre sí.
+		push();
+		rotate(this.angle);
+		// El "translate()" es para dibujar los elipses en
+		// coordenadas relativas a lo que se dibujó anteriormente.
+		translate(this.distance, 0);
+		fill(255);
 		ellipse(0, 0, this.radius*2, this.radius*2);
+		if (this.planets != null) {
+			for (var i = 0; i < this.planets.length; i++) {
+				this.planets[i].show();
+			}
+		}
+		pop();
 	}
 }
