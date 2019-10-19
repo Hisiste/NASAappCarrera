a91e013aadfef561b88fe1ebfbc21eb42195bbd0
R-15-Ricardo <ricardo.reyes.1907@gmail.com>
2019-10-19 15:54:17 -0500

    Cambios a wait y survival mode

--- WebPage/scripts/widget.js
+++ WebPage/scripts/widget.js
@@ -1,25 +1,28 @@
 let a,b,c;
 function setup() {
-  createCanvas(380, 100);
+  let myCanvas=createCanvas(380, 100);
+  myCanvas.parent("widgetHolder");
+  myCanvas.position((windowWidth - width) / 2, (windowHeight - height) / 2);
+
   noStroke();
-  frameRate(5);
-  a=color(255);
-  b=color(255);
-  c=color(255);
+  frameRate(3);
+  a=color(255, 253, 166);
+  b=color(255, 253, 166);
+  c=color(255, 253, 166);
 }
 i=0;
 function draw() {
-  c=b;  
+  c=b;
   b=a;
     clear();
     if(i%3==0){
-        a=color(61);
+        a=color(255, 253, 166);
       }
     else if(i%3==1){
-        a=color(123);
+        a=color(255/3, 253/3, 166/3);
       }
     else{
-        a=color(255);
+        a=color(255/6, 253/6, 166/6);
       }
   fill(a);
   ellipse(70,50,100,100);
